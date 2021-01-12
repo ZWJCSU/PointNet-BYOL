00ecda99fdf3da5d37c61c11d43a71c3cfacb74e
wenjie zhu <43605711+ZWJCSU@users.noreply.github.com>
2021-01-12 09:03:08 +0800

    Update pointnet2_cls_msg.py

--- models/pointnet2_cls_msg.py
+++ models/pointnet2_cls_msg.py
@@ -37,7 +37,7 @@ class get_model(nn.Module):
         x = self.drop1(F.relu(self.bn1(self.fc1(x))))
         x = self.drop2(F.relu(self.bn2(self.fc2(x))))
 #         x = self.fc3(x)
-        x = F.log_softmax(x, -1)
+#         x = F.log_softmax(x, -1)
         return self.projetion(x),l3_points
 
 
